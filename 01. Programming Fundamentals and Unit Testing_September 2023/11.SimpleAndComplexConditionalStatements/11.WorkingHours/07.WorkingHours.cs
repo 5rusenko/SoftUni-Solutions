46c5b78f5d188f9b489b100bdb0d7aaa8e8eafd3
5rusenko <m.s.petrusenko@gmail.com>
2023-10-05 10:22:34 +0300

    new tasks added

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/11.SimpleAndComplexConditionalStatements/11.WorkingHours/07.WorkingHours.cs
@@ -0,0 +1,39 @@
+﻿namespace _11.WorkingHours;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int workingHours = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        if (workingHours >=10 && workingHours <=18 && day=="Monday")
+        {
+            Console.WriteLine("open");
+        }
+        else if (workingHours >= 10 && workingHours <= 18 && day == "Tuesday")
+        {
+            Console.WriteLine("open");
+        }
+        else if (workingHours >= 10 && workingHours <= 18 && day == "Wednesday")
+        {
+            Console.WriteLine("open");
+        }
+        else if (workingHours >= 10 && workingHours <= 18 && day == "Thursday")
+        {
+            Console.WriteLine("open");
+        }
+        else if (workingHours >= 10 && workingHours <= 18 && day == "Friday")
+        {
+            Console.WriteLine("open");
+        }
+        else if (workingHours >= 10 && workingHours <= 18 && day == "Saturday")
+        {
+            Console.WriteLine("open");
+        }
+        else
+        {
+            Console.WriteLine("closed");
+        }
+    }
+}
+
