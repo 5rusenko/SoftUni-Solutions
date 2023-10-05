7144d3b0e0842890520dc9e97e521aca41d0594c
5rusenko <m.s.petrusenko@gmail.com>
2023-10-05 11:32:38 +0300

    final task added to directory 11

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/11.SimpleAndComplexConditionalStatements/10.SummerClothing/10.SummerClothing.cs
@@ -0,0 +1,58 @@
+﻿namespace _10.SummerClothing;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int degrees = int.Parse(Console.ReadLine());
+        string timeOfDay = Console.ReadLine();
+        string clothing = "", shoes = "";
+
+        if (timeOfDay == "Morning")
+        {
+            if (10 <= degrees && degrees <= 18)
+            {
+                clothing = "Sweatshirt";
+                shoes = "Sneakers";
+            }
+            else if (18 < degrees && degrees <= 24)
+            {
+                clothing = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if (degrees >= 25)
+            {
+                clothing = "T-Shirt";
+                shoes = "Sandals";
+            }
+        }
+        else if (timeOfDay == "Afternoon")
+        {
+            if (10 <= degrees && degrees <= 18)
+            {
+                clothing = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if (18 < degrees && degrees <= 24)
+            {
+                clothing = "T-Shirt";
+                shoes = "Sandals";
+            }
+            else if (degrees >= 25)
+            {
+                clothing = "Swim Suit";
+                shoes = "Barefoot";
+            }
+        }
+        else if (timeOfDay == "Evening")
+        {
+            clothing = "Shirt";
+            shoes = "Moccasins";
+        }
+
+        Console.WriteLine($"It's {degrees} degrees, get your {clothing} and {shoes}.");
+
+    }
+
+
+}
+
