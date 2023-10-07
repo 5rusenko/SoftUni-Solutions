d4dc094947ab1f31ece4ce056b24107be6addd97
5rusenko <m.s.petrusenko@gmail.com>
2023-10-07 23:03:10 +0300

    Adding 02.FirstNNumbersSum to 12.

--- 01. Programming Fundamentals and Unit Testing_September 2023/12.ForLoop_Lab/02.FirstNNumbersSum/02.FirstNNumbersSum.cs
+++ 01. Programming Fundamentals and Unit Testing_September 2023/12.ForLoop_Lab/02.FirstNNumbersSum/02.FirstNNumbersSum.cs
@@ -3,7 +3,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 1; i <= num; i += 1)
+        {
+            if (i == 1)
+            {
+                Console.Write(i);
+            }
+            else
+            {
+                Console.Write("+" + i);
+            }
+
+            sum += i;
+        }
+        
+
+        Console.WriteLine("="+sum);
     }
 }
 
