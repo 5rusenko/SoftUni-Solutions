424389fc2d326a08e31d2429b25099dca76cf31b
5rusenko <m.s.petrusenko@gmail.com>
2023-10-10 21:12:49 +0300

    Adding tasks 01-03 to 16.Methods_Lab

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/16.Methods_Lab/01.SignOfIntNum/01.SignOfIntNum.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.SignOfIntNum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        if (num ==0)
+        {
+            Console.WriteLine($"The number {num} is zero.");
+        }
+        else if (num >0)
+        {
+            Console.WriteLine($"The number {num} is positive.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {num} is negative.");
+        }
+    }
+}
+
---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/16.Methods_Lab/01.SignOfIntNum_Method/01.SignOfIntNum_Method.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.SignOfIntNum_Method;
+class Program
+{
+    static void PrintNumSign(int num)
+    {
+        if (num == 0)
+        {
+            Console.WriteLine($"The number {num} is zero.");
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine($"The number {num} is positive.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {num} is negative.");
+        }
+    }
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        PrintNumSign(num);
+    }
+}
+
