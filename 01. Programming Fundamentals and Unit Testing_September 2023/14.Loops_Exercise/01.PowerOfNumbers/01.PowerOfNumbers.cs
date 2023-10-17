b210699718491fa8a37824ea61379a775118d215
5rusenko <m.s.petrusenko@gmail.com>
2023-10-17 09:53:12 +0300

    Adding new lesson directory - 14.Loops_Exercise

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/14.Loops_Exercise/01.PowerOfNumbers/01.PowerOfNumbers.cs
@@ -0,0 +1,9 @@
+﻿int num = int.Parse(Console.ReadLine());
+int pow = int.Parse(Console.ReadLine());
+int result = 0;
+
+for (int i = 0; i<=pow; i+=1)
+{
+    result = num * i;
+}
+Console.WriteLine(result);
