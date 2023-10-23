600a3ec77237167940e07b9f87d0096749b5e99a
5rusenko <m.s.petrusenko@gmail.com>
2023-10-23 18:25:27 +0200

    Adding task 08 to Loops

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/14.Loops_Exercise/08.AccountBalance/08.AccountBalance.cs
@@ -0,0 +1,21 @@
+﻿string input = Console.ReadLine();
+double balance = 0;
+
+while (input!="End")
+{
+    double money = double.Parse(input);
+
+    if(money>=0)
+    {
+        balance += money;
+        Console.WriteLine($"Increase: {money:F2}");
+    }
+    else
+    {
+        balance -= Math.Abs(money);
+        Console.WriteLine($"Decrease: {Math.Abs(money):f2}");
+    }
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Balance: {balance:f2}");
