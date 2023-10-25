6f1bf886fb2f77271f5adf41da967c348c1284e7
5rusenko <m.s.petrusenko@gmail.com>
2023-10-25 16:10:02 +0200

    adding more tasks

---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/15.NestedLoops_Lab/06.TravelSavings/06.TravelSavings.cs
@@ -0,0 +1,16 @@
+﻿string destination = Console.ReadLine();
+
+while (destination!="End")
+{
+    double neededSum = double.Parse(Console.ReadLine());
+    double collectedsum = 0;
+
+    while (collectedsum<neededSum)
+    {
+        double sumtobeadded = double.Parse(Console.ReadLine());
+        collectedsum += sumtobeadded;
+        Console.WriteLine($"Collected: {collectedsum:f2}");
+    }
+    Console.WriteLine($"Going to {destination}!");
+    destination = Console.ReadLine();
+}
---
+++ 01. Programming Fundamentals and Unit Testing_September 2023/15.NestedLoops_Lab/07.SumOfDigitsCalculator/07.SumOfDigitsCalculator.cs
@@ -0,0 +1,17 @@
+﻿string command = Console.ReadLine();
+
+while(command!="End")
+{
+    int inputNum = int.Parse(command);
+    int sum = 0;
+
+    for (int number = inputNum; number>0; number/=10)
+    {
+        int digit = number % 10;
+        sum += digit;
+    }
+    Console.WriteLine($"Sum of digits = {sum}");
+
+    command = Console.ReadLine();
+}
+Console.WriteLine("Goodbye");
